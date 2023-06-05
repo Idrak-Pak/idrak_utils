d5ffac39863521ad0ffafffed6e1344c994c7ef4
Mohammad Ali Abbas <vynz420@gmail.com>
2023-06-05 08:06:18 -0400

    most similar item finding logic

--- idrak_utils.py
+++ idrak_utils.py
@@ -1,5 +1,38 @@
 from num2words import num2words
 import re
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+
+# Example menu items
+# menu = ["zinger burger","chicken wings","pepsi ","2 litter pepsi","chicken fajita pizza","chicken supreme pizza"]
+
+def search_menu(item='',menu=[],threshold=60):
+    '''
+    To match item from menu or search over menu use this function
+    If exact match not found it will return the most similar one
+
+    ;params:
+    item(str): query item
+    menu(list): possible menu a vocab to search
+    threshold(int): how much things should be similar 
+
+    return (most_similar_item, scores)
+    '''
+    # Calculate the similarity ratio between the search item and each menu item
+    ratios = process.extract(item, menu, scorer=fuzz.token_sort_ratio)
+
+    # Sort the ratios in descending order
+    ratios = sorted(ratios, key=lambda x: x[1], reverse=True)
+
+    # Get the most similar menu item and its ratio
+    most_similar = ratios[0]
+
+    # Check if the similarity ratio is above a certain threshold
+    if most_similar[1] >= 60:
+        return most_similar[0],most_similar[1]
+    else:
+        return "Item not found",most_similar[1]
+
 
 def convert_numbers_to_words(string):
     words = []
@@ -9,6 +42,7 @@ def convert_numbers_to_words(string):
         else:
             words.append(word)
     return ' '.join(words)
+
 
 # Example usage
 string = "House 89, Ward 6 Talagang"
